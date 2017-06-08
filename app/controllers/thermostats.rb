3692bc64d13ce7e3ba2647957fc711b2e9962342
Mário Ribeiro <mail@marioribeiro.com>
2017-06-08 21:50:29 +0100

    Add return messages for the routes
    - Initial work for exceptions

--- app/controllers/thermostats.rb
+++ app/controllers/thermostats.rb
@@ -10,37 +10,64 @@ class ThermostatAPI < Sinatra::Base
   end
 
   get '/thermostats' do
-    content_type :json
-    Thermostat.all.to_json
+    return_message = {}
+    if Thermostat.count == 0
+      return_message[:status] = 'success'
+      return_message[:thermostats] = 'there are no thermostats'
+    else
+      return_message[:status] = 'success'
+      return_message[:thermostats] = Thermostat.all
+    end
+    return_message.to_json
   end
 
   post '/thermostats' do
+    return_message = {}
     content_type :json
     user = User.authenticate(params[:user_id], params[:api_key])
     if user
       session[:user_id] = user.id
-      Thermostat.create(user_id: session[:user_id]).to_json
+      thermostat = Thermostat.create(user_id: session[:user_id])
+      return_message[:status] = 'sucess'
+      return_message[:thermostat] = thermostat
     else
-      "Authentication Error - Check your User Id & API Key".to_json
+      return_message[:status] = 'error'
+      return_message[:message] = 'Please check your User ID and API Key'
+      return_message.to_json
     end
   end
 
   get '/thermostats/:id' do
-    Thermostat.get(params[:id]).to_json
+    return_message = {}
+    thermostat = Thermostat.get(params[:id])
+    if thermostat
+      return_message[:status] = 'sucess' 
+      return_message[:thermostat] = thermostat
+    else
+      return_message[:status] = 'error'
+      return_message[:message] = 'Invalid ID. Thermostat not found'
+    end
+    return_message.to_json
   end
 
   post '/thermostats/:id/temperature/increase' do
+    return_message = {}
     @thermostat = Thermostat.get(params[:id])
     current_temperature = get_current_temperature
     @thermostat.update(:temperature => current_temperature += 1)
-    @thermostat.to_json
+    return_message[:status] = 'success'
+    return_message[:thermostat] = @thermostat
+    return_message.to_json
   end
 
   post '/thermostats/:id/temperature/decrease' do
+    return_message = {}
     @thermostat = Thermostat.get(params[:id])
     current_temperature = get_current_temperature
     @thermostat.update(:temperature => current_temperature -= 1)
-    @thermostat.to_json
+    return_message[:status] = 'success'
+    return_message[:thermostat] = @thermostat
+    return_message.to_json
   end
 
   
