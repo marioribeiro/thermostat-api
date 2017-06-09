81a6ee4f1d2966dbf3e2f923ec2713bf42fbe66e
Mário Ribeiro <mail@marioribeiro.com>
2017-06-09 10:12:52 +0100

    Add power_save_mode and max temperatures

--- app/controllers/thermostats.rb
+++ app/controllers/thermostats.rb
@@ -1,7 +1,15 @@
 class ThermostatAPI < Sinatra::Base
 
+  MAX_TEMP_PSM = 25
+  MAX_TEMP = 32
+
   before do
     response.headers['Access-Control-Allow-Origin'] = '*'
+  end
+
+  def power_save_mode?
+    thermostat = Thermostat.get(@thermostat.id)
+    thermostat.power_save_mode
   end
 
   def get_current_temperature
@@ -55,9 +63,18 @@ class ThermostatAPI < Sinatra::Base
     return_message = {}
     @thermostat = Thermostat.get(params[:id])
     current_temperature = get_current_temperature
-    @thermostat.update(:temperature => current_temperature += 1)
-    return_message[:status] = 'success'
-    return_message[:thermostat] = @thermostat
+    if power_save_mode? && current_temperature + 1 < MAX_TEMP_PSM
+      @thermostat.update(:temperature => current_temperature += 1)
+      return_message[:status] = 'success'
+      return_message[:thermostat] = @thermostat
+    elsif !power_save_mode? && current_temperature + 1 < MAX_TEMP
+      @thermostat.update(:temperature => current_temperature += 1)
+      return_message[:status] = 'success'
+      return_message[:thermostat] = @thermostat
+    else
+      return_message[:status] = 'error'
+      return_message[:message] = 'thermostat at maximum temperature'
+    end
     return_message.to_json
   end
 
--- app/models/thermostat.rb
+++ app/models/thermostat.rb
@@ -1,8 +1,9 @@
 class Thermostat
   include DataMapper::Resource
 
-  property :id,          Serial
-  property :temperature, Integer, :default => 20
+  property :id,              Serial
+  property :temperature,     Integer, :default => 20
+  property :power_save_mode, Boolean, :default => true
 
   belongs_to :user
 
